79929ad9c3b59b91850a4bc8cccb71fc708b4b62
Selmen Rayen <selmanerayene95@gmail.com>
2024-01-18 01:46:58 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SRayen E-Commerce",
+  description: "Modern E-Commerce Website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer footer-center p-4 bg-base-300 text-base-content">
+      Copyright © 2024 - All right reserved - SRayen - E-commerce
+    </div>
+  );
+}
