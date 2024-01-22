d9a79968861d25cc60605dd11bd8df5b49190edc
Selmen Rayen <selmanerayene95@gmail.com>
2024-01-22 16:38:43 +0100

    Create new users

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,10 +1,17 @@
 import { auth } from "@/lib/auth";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
+import ClientProviders from "./ClientProviders";
 
-export default async function Providers({children}:{
-    children:React.ReactNode
-}){
-    const session=await auth()
-    return <SessionProvider session={session}>{children}</SessionProvider>
+export default async function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  return (
+    <SessionProvider session={session}>
+      <ClientProviders>{children}</ClientProviders>
+    </SessionProvider>
+  );
 }
